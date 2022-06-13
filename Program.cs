390bd509563a1e3859f0d9bcf67b5ac0b54cd032
Ezh+Wine <valvic90@gmail.com>
2022-06-13 22:50:01 +0300

    third task completed

--- Program.cs
+++ Program.cs
@@ -104,18 +104,34 @@
 
 //Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.Write("введите первое число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("введите второе число: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("введите третье число: ");
-int num3 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("введите третье число: ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
 
 
-if (num1 > num2 & num1> num3){
-    Console.Write("максимальное число: " + num1);}
-else if (num2 > num1 & num2> num3){
-    Console.Write("максимальное число: " + num2);}
-else if (num3 > num1 & num3> num2){
-Console.Write("максимальное число: " + num3);}
-else {Console.Write("кажется, введены одинаковые числа");}
+// if (num1 > num2 & num1> num3){
+//     Console.Write("максимальное число: " + num1);
+//     }
+// else if (num2 > num1 & num2> num3){
+//     Console.Write("максимальное число: " + num2);
+//     }
+// else if (num3 > num1 & num3> num2){
+// Console.Write("максимальное число: " + num3);
+// }
+// else {Console.Write("кажется, введены одинаковые числа");
+// }
+
+//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num % 2 == 0){
+//    Console.Write("Введенное число четное"); 
+// }
+// else {
+//     Console.Write("Введенное число нечетное");
+//     }

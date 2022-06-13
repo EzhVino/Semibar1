03f6b5ef389c96558195ca9110ae041627fb84e4
Ezh+Wine <valvic90@gmail.com>
2022-06-13 21:27:37 +0300

    done

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿// Console.Write("input integer number ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int result = num * num;
+// Console.WriteLine("result is " + result);
+
+//Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+// Console.Write("input first number ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("input second number ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 == num2 * num2){
+//     Console.WriteLine("first number is a square of second number");
+// }
+// else {
+//     Console.WriteLine("first number is not a square of second number");
+// }
+
+//Напишите программу, которая будет выдавать название дня недели по заданному номеру
+
+// Console.Write("input number of a day: ");
+// int day = Convert.ToInt32(Console.ReadLine());
+
+// if (day < 1 || day > 7){
+//     Console.Write("incorrect day");
+// }
+// else {
+//     if (day == 1){
+//     Console.WriteLine("monday");    
+//     }
+//      if (day == 2){
+//     Console.WriteLine("tuesday");    
+//     }
+//      if (day == 3){
+//     Console.WriteLine("wednesday");    
+//     }
+//      if (day == 4){
+//     Console.WriteLine("thursday");    
+//     }
+//      if (day == 5){
+//     Console.WriteLine("friday");    
+//     }
+//      if (day == 6){
+//     Console.WriteLine("saturday");    
+//     }
+//      if (day == 7){
+//     Console.WriteLine("sunday");    
+//     }
+// }
+
+//Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+// Console.Write("input N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int current; 
+// if (n < 0){
+//     current = n;
+//     n = n * (-1);
+// }
+// else{
+//     current = -1 * n;
+// }
+// while (current < n -1){
+//     current++;
+//     Console.Write(current + " ");
+// }
+
+
+//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа
+
+Console.Write("input N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 100 || num > 999){
+    Console.Write("invalid");
+}
+else {
+Console.WriteLine(num / 10 % 10);
+}
+
+//if (num % 2 == 0) - то это четное число
